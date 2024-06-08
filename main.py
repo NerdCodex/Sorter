cd30c55662144aa3759eee24fb97d0ff31fc57c9
NerdCodex <subburakesh82@gmail.com>
2024-06-08 19:27:14 +0530

    Awarded Freature Added

--- main.py
+++ main.py
@@ -21,7 +21,10 @@ class MyApp(QMainWindow, Ui_MainWindow):
 
         # Triggers
         self.fileLoad.triggered.connect(self.loadfile)
-        self.deptcomboBox.currentTextChanged.connect(self.comboBoxChanged)
+        self.deptcomboBox.currentTextChanged.connect(self.categoryComboBoxChanged)
+        self.awardedDeptComboBox.currentTextChanged.connect(self.awardedComboBoxChanged)
+        self.startYearComboBox.currentTextChanged.connect(self.startYearComboBoxChanged)
+        self.endYearComboBox.currentTextChanged.connect(self.endYearComboBoxChanged)
         self.categoryExportAll.clicked.connect(self.exportall)
         self.categoryExport.clicked.connect(self.export)
     
@@ -37,23 +40,84 @@ class MyApp(QMainWindow, Ui_MainWindow):
             self.show_warning_message_box("You Have Opened an Unknow file with Unknown sheets")
     
     # Widget Update Functions
+    def disconnect_signals(self):
+        self.deptcomboBox.currentTextChanged.disconnect(self.categoryComboBoxChanged)
+        self.awardedDeptComboBox.currentTextChanged.disconnect(self.awardedComboBoxChanged)
+        self.startYearComboBox.currentTextChanged.disconnect(self.startYearComboBoxChanged)
+        self.endYearComboBox.currentTextChanged.disconnect(self.endYearComboBoxChanged)
+
+    def reconnect_signals(self):
+        self.deptcomboBox.currentTextChanged.connect(self.categoryComboBoxChanged)
+        self.awardedDeptComboBox.currentTextChanged.connect(self.awardedComboBoxChanged)
+        self.startYearComboBox.currentTextChanged.connect(self.startYearComboBoxChanged)
+        self.endYearComboBox.currentTextChanged.connect(self.endYearComboBoxChanged)
+
+
     def update_combo_box(self, sheets):
         self.deptcomboBox.clear()
+        self.awardedDeptComboBox.clear()
         for sheet in sheets:
             self.deptcomboBox.addItem(sheet)
+            self.awardedDeptComboBox.addItem(sheet)
     
-    def comboBoxChanged(self, value):
+    def categoryComboBoxChanged(self, value):
         if value:
-            self.database.update_table(value, self.maleValueTable, self.femaleValueTable)
+            self.database.update_male_female_table(value, self.maleValueTable, self.femaleValueTable)
     
+    def awardedComboBoxChanged(self, value):
+        if value:
+            self.disconnect_signals()
+            self.updateYearsComboBox(value)
+            self.reconnect_signals()
+            start_year = self.startYearComboBox.currentText()
+            end_year = self.endYearComboBox.currentText()
+            if start_year and end_year:
+                start_year = int(start_year)
+                end_year = int(end_year)
+                self.database.update_awarded_table(value, self.awardedValueTable, start_year, end_year)
+            
+        
+    def startYearComboBoxChanged(self, value):
+        if value:
+            try:
+                sheet_name = self.awardedDeptComboBox.currentText()
+                end_year_text = self.endYearComboBox.currentText()
+                if end_year_text:
+                    end_year = int(end_year_text)
+                    self.database.update_awarded_table(sheet_name, self.awardedValueTable, int(value), end_year)
+            except ValueError:
+                self.show_warning_message_box("Invalid year format. Please select a valid year.")
+
+    def endYearComboBoxChanged(self, value):
+        if value:
+            try:
+                sheet_name = self.awardedDeptComboBox.currentText()
+                start_year_text = self.startYearComboBox.currentText()
+                if start_year_text:
+                    start_year = int(start_year_text)
+                    self.database.update_awarded_table(sheet_name, self.awardedValueTable, start_year, int(value))
+            except ValueError:
+                self.show_warning_message_box("Invalid year format. Please select a valid year.")
+        
+    def updateYearsComboBox(self, value):
+        self.startYearComboBox.clear()
+        self.endYearComboBox.clear()
+        years_list = self.database.awarded_sort.awardedList.departments[value].years_list
+        years_list.sort()
+        for year in years_list:
+            self.startYearComboBox.addItem(str(year))
+            self.endYearComboBox.addItem(str(year))
     
     # Config Functions
     def setQTableWidgetConfig(self):
         self.maleValueTable.setEditTriggers(QAbstractItemView.NoEditTriggers)
         self.femaleValueTable.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.awardedValueTable.setEditTriggers(QAbstractItemView.NoEditTriggers)
 
         self.maleValueTable.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
         self.maleValueTable.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        self.awardedValueTable.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
+        self.awardedValueTable.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
 
         self.femaleValueTable.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
         self.femaleValueTable.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
@@ -77,12 +141,12 @@ class MyApp(QMainWindow, Ui_MainWindow):
             table_file.add_heading("Male")
             male_table = table_file.create_table()
             for index, sheet in enumerate(self.sheet_names):
-                table_file.insert_data(self.database.maleDataExporter(sheet, index+1), male_table)
+                table_file.insert_data(self.database.maleCategoryDataExporter(sheet, index+1), male_table)
             
             table_file.add_heading("\nFemale")
             female_table = table_file.create_table()
             for index, sheet in enumerate(self.sheet_names):
-                table_file.insert_data(self.database.femaleDataExporter(sheet, index+1), female_table)
+                table_file.insert_data(self.database.femaleCategoryDataExporter(sheet, index+1), female_table)
             document.save(filename)
         
     def export(self):
@@ -95,17 +159,12 @@ class MyApp(QMainWindow, Ui_MainWindow):
 
             table_file.add_heading("Male")
             male_table = table_file.create_table()
-            table_file.insert_data(self.database.maleDataExporter(current_sheet_name, 1), male_table)
+            table_file.insert_data(self.database.maleCategoryDataExporter(current_sheet_name, 1), male_table)
 
             table_file.add_heading("\nFemale")
             female_table = table_file.create_table()
-            table_file.insert_data(self.database.femaleDataExporter(current_sheet_name, 1), female_table)
+            table_file.insert_data(self.database.femaleCategoryDataExporter(current_sheet_name, 1), female_table)
             document.save(filename)
-            
-        
-
-            
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- pgms/database.py
+++ pgms/database.py
@@ -1,5 +1,72 @@
 import pandas as pd
-from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem
+from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem, QMessageBox
+
+class AwardedTime:
+    def __init__(self):
+        self.part_time = 0
+        self.full_time = 0
+    
+    def loadTime(self, content):
+        self.part_time = len(content.query('`P.T / F.T` == "Part-Time"').to_numpy().tolist())
+        self.full_time = len(content.query('`P.T / F.T` == "Full-Time"').to_numpy().tolist())
+    
+    def getTime(self):
+        return [self.part_time, self.full_time]
+    
+    def __add__(self, object):
+        temp = AwardedTime()
+        temp.part_time = self.part_time + object.part_time
+        temp.full_time = self.full_time + object.full_time
+        return temp
+
+class AwardedGender:
+    def __init__(self):
+        self.male = AwardedTime()
+        self.female = AwardedTime()
+    
+    def loadGender(self, content):
+        self.male.loadTime(content.query('Gender == "Male"'))
+        self.female.loadTime(content.query('Gender == "Female"'))
+
+class AwardedYear:
+    def __init__(self, years):
+        self.years = {}
+        self.years_list = years
+        for year in years:
+            self.years[year] = AwardedGender()
+    
+    def loadAwardedYear(self, content):
+        content['Commencement Date'] = pd.to_datetime(content['Commencement Date'])
+        for year in self.years_list:
+            self.years[year].loadGender(content.query(f'Status == "Awarded" and `Commencement Date`.dt.year == {year}'))
+
+class AwardedDepartment:
+    def __init__(self, sheets, content):
+        self.departments = {}
+        self.content = content
+        self.sheets = sheets
+        for sheet in sheets:
+            year = self.getYears(self.content.parse(sheet)["Commencement Date"].to_list())
+            self.departments[sheet] = AwardedYear(year)
+        self.loadDepartment()
+    
+    def loadDepartment(self):
+        for sheet in self.sheets:
+            current_content = self.content.parse(sheet)
+            self.departments[sheet].loadAwardedYear(current_content)
+    
+    def getYears(self, timestamps):
+        years = []
+        for timestamp in timestamps:
+            if pd.notna(timestamp):
+                years.append(timestamp.year)
+        return list(set(years))
+
+class AwardedSort:
+    def __init__(self, fileContent):
+        self.fileContent = fileContent
+        self.awardedList = AwardedDepartment(self.fileContent.sheet_names, fileContent)
+
 
 class Caste:
     def __init__(self):
@@ -27,6 +94,9 @@ class Religion:
         self.hindu.loadCaste(content.query('Religion == "Hindu"'))
         self.christian.loadCaste(content.query('Religion == "Christian"'))
         self.muslim.loadCaste(content.query('Religion == "Muslim"'))
+    
+    def exportReligion(self):
+        return [self.hindu.exportCaste(), self.christian.exportCaste(), self.muslim.exportCaste()]
 
 class Gender:
     def __init__(self):
@@ -60,48 +130,72 @@ class Database:
     def __init__(self, filename:str):
         self.file_content = pd.ExcelFile(filename)
         self.category_sort = CategorySort(self.file_content)
+        self.awarded_sort = AwardedSort(self.file_content)
 
-    def maleDataExporter(self, sheet_name, index):
-
-        hindu = [index, sheet_name, "Hindu"]
-        christian = [index, sheet_name, "Christian"]
-        muslim = [index, sheet_name, "Muslim"]
-
-        hindu.extend(self.category_sort.department.departments[sheet_name].male.hindu.exportCaste())
-        christian.extend(self.category_sort.department.departments[sheet_name].male.christian.exportCaste())
-        muslim.extend(self.category_sort.department.departments[sheet_name].male.muslim.exportCaste())
-
-        return [hindu, christian, muslim]
-    
-    def femaleDataExporter(self, sheet_name, index):
+    def maleCategoryDataExporter(self, sheet_name, index):
 
         hindu = [index, sheet_name, "Hindu"]
         christian = [index, sheet_name, "Christian"]
         muslim = [index, sheet_name, "Muslim"]
 
-        hindu.extend(self.category_sort.department.departments[sheet_name].female.hindu.exportCaste())
-        christian.extend(self.category_sort.department.departments[sheet_name].female.christian.exportCaste())
-        muslim.extend(self.category_sort.department.departments[sheet_name].female.muslim.exportCaste())
+        religion = self.category_sort.department.departments[sheet_name].male.exportReligion()
+
+        hindu.extend(religion[0])
+        christian.extend(religion[1])
+        muslim.extend(religion[2])
 
         return [hindu, christian, muslim]
+    
+    def femaleCategoryDataExporter(self, sheet_name, index):
 
+        hindu = [index, sheet_name, "Hindu"]
+        christian = [index, sheet_name, "Christian"]
+        muslim = [index, sheet_name, "Muslim"]
+
+        religion = self.category_sort.department.departments[sheet_name].female.exportReligion()
+
+        hindu.extend(religion[0])
+        christian.extend(religion[1])
+        muslim.extend(religion[2])
+
+        return [hindu, christian, muslim]
     
-    
-    def update_table(self, dept, maleTable:QTableWidget, femaleTable:QTableWidget):
+    def update_male_female_table(self, dept, maleTable:QTableWidget, femaleTable:QTableWidget):
         # Male
-        hindu = self.category_sort.department.departments[dept].male.hindu.exportCaste()
-        christian = self.category_sort.department.departments[dept].male.christian.exportCaste()
-        muslim = self.category_sort.department.departments[dept].male.muslim.exportCaste()
-
-        for x , religion in enumerate([hindu, christian, muslim]):
+        for x , religion in enumerate(self.category_sort.department.departments[dept].male.exportReligion()):
             for y, value in enumerate(religion):
                 maleTable.setItem(x, y, QTableWidgetItem(str(value)))
 
         # Female
-        hindu = self.category_sort.department.departments[dept].female.hindu.exportCaste()
-        christian = self.category_sort.department.departments[dept].female.christian.exportCaste()
-        muslim = self.category_sort.department.departments[dept].female.muslim.exportCaste()
-
-        for x , religion in enumerate([hindu, christian, muslim]):
+        for x , religion in enumerate(self.category_sort.department.departments[dept].female.exportReligion()):
             for y, value in enumerate(religion):
                 femaleTable.setItem(x, y, QTableWidgetItem(str(value)))
+    
+    def update_awarded_table(self, value, table:QTableWidget, start_year, end_year):
+        maleTime = AwardedTime()
+        femaleTime = AwardedTime()
+        if start_year == end_year:
+            maleTime = self.awarded_sort.awardedList.departments[value].years[start_year].male
+            femaleTime = self.awarded_sort.awardedList.departments[value].years[start_year].female
+
+        elif start_year < end_year:
+            years_list = self.awarded_sort.awardedList.departments[value].years_list
+            try:
+                for year in range(start_year, end_year+1):
+                    if year in years_list:
+                        maleTime = maleTime + self.awarded_sort.awardedList.departments[value].years[year].male
+                        femaleTime = femaleTime + self.awarded_sort.awardedList.departments[value].years[year].female
+            except KeyError:
+                print("Not Found")
+
+        elif start_year > end_year:
+            warning_box = QMessageBox()
+            warning_box.setIcon(QMessageBox.Warning)
+            warning_box.setWindowTitle("Warning")
+            warning_box.setText(f"Starting Year {start_year} is greater than ending year {end_year}")
+            warning_box.setStandardButtons(QMessageBox.Ok)
+            warning_box.exec_()
+
+        for x, gender in enumerate([maleTime, femaleTime]):
+            for y, time in enumerate(gender.getTime()):
+                table.setItem(x,y,QTableWidgetItem(str(time)))
